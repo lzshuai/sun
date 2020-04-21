3240f34d77c770fd78013fccb4c33e8d3695c55b
lzshuai <1406953818@qq.com>
2020-04-21 23:20:19 +0800

    测试

---
+++ src/views/fun/DebounceTest.vue
@@ -0,0 +1,33 @@
+<template>
+ <div>
+
+     <button @click="debounceTest"> debounce</button>
+     <br>
+     <div style="width: 30px;height:30px;border: 1px solid red" @click="testDD"> div触发</div>
+ </div>
+</template>
+
+<script>
+    import {debounce} from 'lodash'
+    export default {
+        name: "DebounceTest",
+        methods:{
+            debounceTest(){
+                console.log(111)
+                debounce(function () {
+                    console.log("1s打印一次")
+                },1000)
+            },
+            testDD: debounce(function() {
+                console.log("testDD")
+            },1000),
+
+
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
